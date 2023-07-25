af7b1d8a9bafa64f649046e4887913342275f570
ganesh <gv975796@gmail.com>
2023-07-25 17:01:00 +0530

    Review changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const mainDiv = document.getElementById('main-div');
 mainDiv.style.display = 'none';
 function animateSmallDiv() {
@@ -5,25 +6,23 @@ function animateSmallDiv() {
   const largeDiv = document.querySelector('.large-div');
   const animationDiv = document.querySelector('.animationDiv');
   const para = document.querySelector('.para');
+  const capture = document.getElementById('capture');
 
   smallDiv.classList.add('hidden-small-div');
+  capture.classList.add('add-hover');
   if (smallDiv.classList.contains('hidden-small-div')) {
-    animationDiv.style.left = "-730px";
-    animationDiv.style.top = "280px";
+    animationDiv.style.left = "-687px";
+    animationDiv.style.top = "300px";
     animationDiv.style.width = '340px';
     animationDiv.style.height = '30%';
     animationDiv.style.flexWrap = 'wrap';
     mainDiv.style.display = 'block';
-    mainDiv.style.transition = 'all 0.5s';
-    mainDiv.style.transition = 'left 1s ease';
-    mainDiv.style.transformOrigin = 'left';
     mainDiv.style.zIndex = '2';
-    smallDiv.style.width = '40%'
-    const paraHeight = para.scrollHeight;
-    para.style.top = `-${paraHeight}px`;
+    smallDiv.style.width = '40%';
     setTimeout(() => {
-      para.style.top = "0";
-    }, 10);
+         mainDiv.style.opacity = '1';
+      mainDiv.style.transform = 'translateX(0)';
+    }, 1000);
 
   }
 }
@@ -40,7 +39,8 @@ function back() {
   animationDiv.style.height = 'fit-content';
   animationDiv.style.flexWrap = 'revert';
   mainDiv.style.display = 'none';
-
-  const paraHeight = para.scrollHeight;
-  para.style.top = `-${paraHeight}px`;
+  mainDiv.style.transform = 'translateX(100%)';
+  setTimeout(() => {
+    mainDiv.style.display = 'none';
+  }, 500); 
 }
